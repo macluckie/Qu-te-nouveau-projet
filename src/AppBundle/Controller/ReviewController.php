8ef369fe821240d22a4d0b2d8cd09d5d282660a1
macluckie <dimitri.macluckie@gmail.com>
2018-05-24 14:39:41 +0200

    formulaireok

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -7,50 +7,140 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Form\ReviewType;
+
 
 /**
  * @Route("/review")
  */
 class ReviewController extends Controller
 {
-    /**
-     * @Route("/"), name="index_review"
-     */
-    public function indexAction()
-    {
-        $em = $this->getDoctrine()-> getManager();
-        $reviews = $em->getRepository('AppBundle:Review')-> findAll();
-        return $this->render('review\review.html.twig', array(
-          'reviews'=>$reviews
-        ));
-    }
+  /**
+* Lists all review entities.
+*
+* @Route("/", name="review_index")
+* @Method("GET")
+*/
+
+public function indexAction()
+{
+    $em = $this->getDoctrine()->getManager();
+
+    $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+    return $this->render('review/index.html.twig', array(
+        'reviews' => $reviews,
+    ));
+}
 
 
 
-    /**
-     * @Route("/new"), name="new_review"
-     */
-    public function newAction(Request $request)
-    {
+/**
+ * Creates a new review entity.
+ *
+ * @Route("/new", name="review_new")
+ * @Method({"GET", "POST"})
+ */
 
-        $review = new Review();
-        $form = $this->createForm('AppBundle\Form\ReviewType', $review);
-          $form->handleRequest($request);
+ public function newAction(Request $request)
+ {
+         $review = new Review();
+         $form = $this->createForm(ReviewType::class, $review);
+
+         // build the form ...
+
+ $form->handleRequest($request);
+
+ if ($form->isSubmitted() && $form->isValid()) {
+     $em = $this->getDoctrine()->getManager();
+     $em->persist($review);
+     $em->flush();
+
+     // You can use too :
+     // return $this->redirect($this->generateUrl('review_show', array('id' => $review->getId())))
+
+     return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+     }
+
+ return $this->render('review/new.html.twig', array(
+     'review' => $review,
+     'form' => $form->createView(),
+ ));
+ }
 
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($review);
-            $em->flush();
+ /**
+  * Finds and displays a review entity.
+  *
+  * @Route("show/{id}", name="review_show")
+  * @Method("GET")
+  */
+ public function showAction(Review $review)
+ {
+     $deleteForm = $this->createDeleteForm($review);
 
-            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
-        }
+     return $this->render('review/show.html.twig', array(
+         'review' => $review,
+         'delete_form' => $deleteForm->createView(),
+     ));
+ }
 
 
-        return $this->render('review\new.html.twig', array(
-          'review' => $review,
-          'form' => $form->createView()
+ /**
+  * Displays a form to edit an existing review entity.
+  *
+  * @Route("/{id}/edit", name="review_edit")
+  * @Method({"GET", "POST"})
+  */
+ public function editAction(Request $request, Review $review)
+ {
+     $deleteForm = $this->createDeleteForm($review);
+     $editForm = $this->createForm('AppBundle\Form\ReviewType', $review);
+     $editForm->handleRequest($request);
 
-        ));
-    }
+     if ($editForm->isSubmitted() && $editForm->isValid()) {
+         $this->getDoctrine()->getManager()->flush();
+
+         return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+     }
+
+     return $this->render('review/edit.html.twig', array(
+         'review' => $review,
+         'edit_form' => $editForm->createView(),
+         'delete_form' => $deleteForm->createView(),
+     ));
+ }
+
+ private function createDeleteForm(Review $review)
+ {
+     return $this->createFormBuilder()
+         ->setAction($this->generateUrl('review_delete', array('id' => $review->getId())))
+         ->setMethod('DELETE')
+         ->getForm()
+     ;
+ }
+
+ /**
+  * Deletes a review entity.
+  *
+  * @Route("/{id}", name="review_delete")
+  * @Method("DELETE")
+  */
+ public function deleteAction(Request $request, Review $review)
+ {
+     $form = $this->createDeleteForm($review);
+     $form->handleRequest($request);
+
+     if ($form->isSubmitted() && $form->isValid()) {
+         $em = $this->getDoctrine()->getManager();
+         $em->remove($review);
+         $em->flush();
+     }
+
+     return $this->redirectToRoute('review_index');
+ }
+
+
+
+
 }
--- src/AppBundle/Form/ReviewType.php
+++ src/AppBundle/Form/ReviewType.php
@@ -5,30 +5,49 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
 
 class ReviewType extends AbstractType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('text')->add('note')->add('userRated')->add('publicationDate')->add('reviewAuthor');
-    }/**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Review'
-        ));
-    }
+  /**
+* {@inheritdoc} Including all fields from Review entity.
+*/
 
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'appbundle_flight';
-    }
+public function buildForm(FormBuilderInterface $builder, array $options)
+{
+    $builder
+        ->add('text',  TextareaType::class, array('attr'=>array('maxlength'=>250, 'label'=> 'description')))
+        ->add('publicationDate', DateType::class, array('data'=> new \DateTime('now')))
+        ->add('note', IntegerType::class, array('attr'=>array('min'=>0, 'label'=> 'Note')))
+        ->add('userRated', EntityType::class, array(
+          'class'=>'AppBundle\Entity\User',
+          'query_builder'=> function(EntityRepository $er){
+            return $er->createQueryBuilder('u')
+                      ->orderBy('u.lastName','ASC');
+            },
+            'label'=>'lastName'))
+        ->add('reviewAuthor')
+        ->add('save',SubmitType::class)
+        ->getForm();
+
+
+}
+
+
+/**
+* {@inheritdoc} Targeting Review entity
+*/
+
+public function configureOptions(OptionsResolver $resolver)
+{
+    $resolver->setDefaults(array(
+        'data_class' => 'AppBundle\Entity\Review'
+    ));
+}
+
 }
